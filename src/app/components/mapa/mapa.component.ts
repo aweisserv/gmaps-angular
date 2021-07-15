538b709680af09f1ff1f7978075f269857e5a2e1
Allan Weisser Verdessi <vredes@live.com>
2021-07-15 01:01:32 -0400

    map, location & markers working

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marcador } from './classes/marcador.class';
 
 @Component({
   selector: 'app-mapa',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
-  constructor() { }
+  public marcadores: Marcador[] =[];
+
+  lat = -33.447487;
+  lng = -70.673676;
+
+  constructor() {  }
 
   ngOnInit(): void {
+
+    const nuevoMarcador = new Marcador(-33.447487, -70.673676);
+    this.marcadores.push( nuevoMarcador );
+
+  }
+
+  agregarMarcador( evento: any ) {
+
+    const coords: { lat: number, lng: number } = evento.coords;
+    console.log( evento );
+    console.log( coords.lat, coords.lng );
+
+    const nuevoMarcador = new Marcador( coords.lat, coords.lng );
+    this.marcadores.push( nuevoMarcador );
+
   }
 
 }
